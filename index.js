4bd5ef23fc082dd58239a428cd343106f364559b
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-11 02:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return 'Happy Holidays!';
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy Holidays, ${name}!';  
+}
+
+function happyHolidayTo(holiday, days) {
+  return 'It\'s ${days} days until ${holiday}';
+}
